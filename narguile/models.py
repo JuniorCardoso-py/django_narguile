f77d589f7de0ffdd7fbe05af07d7702854073688
JuniorCardoso-py <juniorcardoso.sc@outlook.com>
2021-06-19 20:57:56 -0300

    created models

--- narguile/models.py
+++ narguile/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from base.models import BaseModel
 
 # Create your models here.
+class Narguile(BaseModel):
+
+    name = models.CharField(max_length=80)
+    price = models.FloatField()
+    heitgt_type = models.CharField(max_length=30)
+    stock = models.IntegerField()
+    image = models.ImageField()
